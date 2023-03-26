ada88a0eef825360ecff7a8734203d499a0d3454
mdraselmiah95 <russelroy15@gmail.com>
2023-03-27 03:47:43 +0600

    Chat App frontend setup

--- frontend/src/routes/routes.js
+++ frontend/src/routes/routes.js
@@ -1,5 +1,23 @@
 import { createBrowserRouter } from "react-router-dom";
+import Chat from "../pages/Chat";
+import Error from "../pages/Error";
+import Register from "../pages/Register";
+import Login from "../pages/Login";
 
-const routes = createBrowserRouter([]);
+const routes = createBrowserRouter([
+  {
+    path: "/",
+    element: <Chat />,
+    errorElement: <Error />,
+  },
+  {
+    path: "/register",
+    element: <Register />,
+  },
+  {
+    path: "/login",
+    element: <Login />,
+  },
+]);
 
 export default routes;
