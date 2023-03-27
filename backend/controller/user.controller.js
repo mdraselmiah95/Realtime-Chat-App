80563fd23c6292b705cd1176598644555cb1ad96
mdraselmiah95 <russelroy15@gmail.com>
2023-03-28 03:40:26 +0600

    WIP on Chat APP Register

--- backend/controller/user.controller.js
+++ backend/controller/user.controller.js
@@ -1,3 +1,26 @@
-module.exports.register = (req, res, next) => {
-  console.log(req.body);
+const User = require("../model/user.model");
+const bcrypt = require("bcrypt");
+
+module.exports.register = async (req, res, next) => {
+  try {
+    const { username, email, password } = req.body;
+    const usernameCheck = await User.findOne({ username });
+    if (usernameCheck)
+      return res.json({ msg: "Username already used", status: false });
+    const emailCheck = await User.findOne({ email });
+    if (emailCheck)
+      return res.json({ meg: "Email already used", status: false });
+
+    const hashPassword = await bcrypt.hash(password, 10);
+    const user = await User.create({
+      username,
+      email,
+      password: hashPassword,
+    });
+    delete user.password;
+    return res.json({ status: true, user });
+  } catch (error) {
+    console.log(error);
+    next();
+  }
 };
---
+++ backend/model/user.model.js
@@ -0,0 +1,32 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: true,
+    min: 3,
+    max: 20,
+    unique: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+    max: 50,
+    min: 5,
+  },
+  isAvatarImageSet: {
+    type: Boolean,
+    default: false,
+  },
+  avatarImage: {
+    type: String,
+    default: "",
+  },
+});
+
+module.exports = mongoose.model("User", userSchema);
