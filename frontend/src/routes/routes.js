8395b5272c7f3058826629cff42fcfd591663384
mdraselmiah95 <russelroy15@gmail.com>
2023-03-29 04:36:13 +0600

    WIP on Chat App Avatar page

--- frontend/src/routes/routes.js
+++ frontend/src/routes/routes.js
@@ -3,12 +3,17 @@ import Chat from "../pages/Chat";
 import Error from "../pages/Error";
 import Register from "../pages/Register";
 import Login from "../pages/Login";
+import SetAvatar from "../components/SetAvatar";
 
 const routes = createBrowserRouter([
   {
     path: "/",
     element: <Chat />,
     errorElement: <Error />,
+  },
+  {
+    path: "/setAvatar",
+    element: <SetAvatar />,
   },
   {
     path: "/register",
