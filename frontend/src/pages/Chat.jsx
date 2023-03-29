a4d27d5774d7ebb8d95d913ed43007061e2047fa
mdraselmiah95 <russelroy15@gmail.com>
2023-03-29 14:49:49 +0600

    WIP on Chat App Chat page

--- frontend/src/pages/Chat.jsx
+++ frontend/src/pages/Chat.jsx
@@ -1,11 +1,42 @@
 import React from "react";
+import styled from "styled-components";
+import Contacts from "../components/Contacts";
+import ChatContainer from "../components/ChatContainer";
+import Welcome from "../components/Welcome";
 
 const Chat = () => {
   return (
-    <div>
-      <h1>This is Chat Home</h1>
-    </div>
+    <Container>
+      <div className="container">
+        <Contacts />
+
+        <Welcome />
+
+        <ChatContainer />
+      </div>
+    </Container>
   );
 };
+
+const Container = styled.div`
+  height: 100vh;
+  width: 100vw;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  gap: 1rem;
+  align-items: center;
+  background-color: #131324;
+  .container {
+    height: 85vh;
+    width: 85vw;
+    background-color: #00000076;
+    display: grid;
+    grid-template-columns: 25% 75%;
+    @media screen and (min-width: 720px) and (max-width: 1080px) {
+      grid-template-columns: 35% 65%;
+    }
+  }
+`;
 
 export default Chat;
